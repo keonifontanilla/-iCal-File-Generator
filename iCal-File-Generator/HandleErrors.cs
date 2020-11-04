394fc41887cf3439878f73a63f45e6f59eeabadb
Keoni Fontanilla <keonifontanilla@gmail.com>
2020-11-03 20:23:05 -1000

    more xml comments

--- iCal-File-Generator/HandleErrors.cs
+++ iCal-File-Generator/HandleErrors.cs
@@ -5,30 +5,40 @@ using System.Windows.Forms;
 
 namespace iCal_File_Generator
 {
+    /// <summary>
+    /// This class handles form errors.
+    /// </summary>
     public static class HandleErrors
     {
         public static string ErrorMsg { get; private set; }
 
-        /***********************************************************************************************
-         * Display error messages
-        ***********************************************************************************************/
+        /// <summary>
+        /// Display error messages.
+        /// </summary>
         public static void DisplayErrorMsg()
         {
             MessageBox.Show(ErrorMsg);
             ErrorMsg = "";
         }
 
-        /***********************************************************************************************
-         * Handles empty required inputs
-        ***********************************************************************************************/
+        /// <summary>
+        /// Handles title errors.
+        /// </summary>
+        /// <param name="input"></param>
         public static void HandleTitleError(string input)
         {
             ErrorMsg += string.IsNullOrWhiteSpace(input) ? "Cannot leave title blank!\n" : "";
         }
 
-        /***********************************************************************************************
-         * Handles empty required inputs
-        ***********************************************************************************************/
+        /// <summary>
+        /// Handles date and time errors.
+        /// </summary>
+        /// <param name="startDatePicker">The start date.</param>
+        /// <param name="startTimePicker">The start time.</param>
+        /// <param name="endTimePicker">The end time.</param>
+        /// <param name="endDatePicker">The end date.</param>
+        /// <param name="dateNow">The current date.</param>
+        /// <param name="recurTime">The recurrence time.</param>
         public static void HandleTimeError(DateTimePicker startDatePicker, DateTimePicker startTimePicker, DateTimePicker endTimePicker, DateTimePicker endDatePicker, DateTime dateNow, DateTime recurTime)
         {
             // Check start time errors
@@ -45,9 +55,11 @@ namespace iCal_File_Generator
                 ? "Update the recurrence date!\n" : "";
         }
 
-        /***********************************************************************************************
-         * Handles invalid email inputs
-        ***********************************************************************************************/
+        /// <summary>
+        /// Handles invalid email inputs.
+        /// </summary>
+        /// <param name="emails">A list of attendee emails.</param>
+        /// <param name="organizer">The organizer's email.</param>
         public static void HandleEmailError(List<string> emails, string organizer)
         {
             Regex rx = new Regex(@"^[-!#$%&'*+/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z{|}~])*@[a-zA-Z](-?[a-zA-Z0-9])*(\.[a-zA-Z](-?[a-zA-Z0-9])*)+$");
@@ -69,9 +81,11 @@ namespace iCal_File_Generator
             }
         }
 
-        /***********************************************************************************************
-         * Trims off milliseconds from date time
-        ***********************************************************************************************/
+        /// <summary>
+        /// Trims off milliseconds from date time.
+        /// </summary>
+        /// <param name="dt">The date and time.</param>
+        /// <returns>Returns a formated date and time.</returns>
         private static DateTime TrimTime(DateTime dt)
         {
             return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second, 0, dt.Kind);
